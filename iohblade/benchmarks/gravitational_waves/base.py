487649f82bd653c1f283640f84c1d376dff55ccd
Ananta Shahane <ananta.shahane@aol.com>
2026-08-06 14:54:17 +0200

    Added Quasi-Universal Interferometer (UIFO) Gravitation Wave Detector Optimiser.

--- iohblade/benchmarks/gravitational_waves/base.py
+++ iohblade/benchmarks/gravitational_waves/base.py
@@ -1,9 +1,13 @@
 import textwrap
 from iohblade.problem import Problem
 
+
 class GravitationalWaveBase(Problem):
-    def __init__(self, dependencies='learn2Design-2026 @ git+https://github.com/artificial-scientist-lab/Learn2Design-2026.git'):
-        super().__init__(name="GravitationalWave")
+    def __init__(self):
+        super().__init__(
+            name="GravitationalWave",
+            dependencies="Learn2Design-2026 git+https://github.com/artificial-scientist-lab/Learn2Design-2026.git@main",
+        )
 
         self.role_prompt = "You are an excellent junior researcher at the CERN."
         self.task_prompt = textwrap.dedent("""
@@ -16,7 +20,7 @@ class GravitationalWaveBase(Problem):
                         random_seed: int | None = None,
                         patience: int | None = None,
                         **kwargs: dict
-                    ) -> None:
+                    ) -> float:
                 ```
                 * Where Objective is a class providing following functionality:
                     * `objective.value(params)` returns the loss of the given configuration.
@@ -29,138 +33,137 @@ class GravitationalWaveBase(Problem):
                     * `objective.budget_exceeded`:	Main loop-termination check (time or evals exhausted).
                     * `objective.evals_since_improvement`: Number of evaluations since last improvement.
                     * `objective.best_loss`: Best fitness till now (minimisation).
-                    * `objective.bounds`:   Per-parameter lows and highs arrays (±inf when unbounded) shaped [[lows], [highs]].
                     * `objective.n_params`: Number of parameters to optimise for.
                 * `kwargs`: are the custom hyperparameters for algorithm you'd write, feel free to add those parameters if needed.
-            * Your goal is to optimise the `objective.params` within the `objective.bounds`, so that the loss of signal measured from `objective.value(params)`
+            * Your goal is to optimise the `objective.params`, so that the loss of signal measured from `objective.value(params)`
             is minimised.
             * Make sure to use self.prepare(obj, unbounded:bool, random_seed=random_seed) on line 1 of the optimize function.
             * Make sure to run objective.warmup_value() and objective.start_logging() at the start of optimisation process.
+            * Make sure to return objective.best_loss at the end of the loop.
             """)
 
         self.example_prompt = textwrap.dedent("""
             * Here is an example program for running a UIFOProblem solver.
             ```python
-                @dataclass
-                class Genome:
-                    params : list[float]
-                    fitness: float = float('nan')
+            @dataclass
+            class Genome:
+                params : list[float]
+                fitness: float = float('nan')
 
-                    def reset_fitness(self):
-                        fitness = float('nan')
-
-
-                class EvolutionaryStrategy(OptimizationAlgorithm):
-
-                    algorithm_str = 'EvolutionaryStrategy'
-
-                    def __init__(self, Lambda=10, Mu=10) -> None:
-                        super()
-                        self.parent_population: list[Genome] = []
-                        self.offspring_population: list[Genome] = []
-                        self.Lambda = Lambda
-                        self.Mu = Mu
-
-                    def Initialise(self, obj: Objective):
-                        parent_population = []
-                        for i in range(self.Lambda):
-                            params = list(obj.random_params())
-                            genome = Genome(params)
-                            parent_population.append(genome)
-                        self.parent_population = parent_population
-
-                    def Mutate(self):
-                        for i in range(len(self.offspring_population)):
-                            individual = self.mutate(self.offspring_population[i])
-                            self.offspring_population[i] = individual
-
-                    def mutate(self, individual: Genome) -> Genome:
-                        params = self.update_params(individual.params)
-                        return_individual = Genome(params)
-                        return return_individual
-
-                    def Crossover(self):
-                        offspring_population = []
-                        for i in range(len(self.parent_population)):
-                            individual = self.crossover(self.parent_population[i], self.parent_population[(i + 1) % len(self.parent_population)])
-                            offspring_population.append(individual)
-
-                        self.offspring_population = offspring_population
+                def reset_fitness(self):
+                    self.fitness = float('nan')
 
 
-                    def crossover(self, parent1: Genome, parent2: Genome):
-                        params = []
-                        for i in range(len(parent1.params)):
-                            weight = random.random()
-                            param = (parent1.params[i] * weight) + (parent2.params[i] * (1 - weight))
-                            params.append(param)
-                        return Genome(params)
+            class GeneticAlgorithm(OptimizationAlgorithm):
+
+                algorithm_str = 'GeneticAlgorithm'
+
+                def __init__(self, Lambda=10, Mu=10) -> None:
+                    super()
+                    self.parent_population: list[Genome] = []
+                    self.offspring_population: list[Genome] = []
+                    self.Lambda = Lambda
+                    self.Mu = Mu
+
+                def Initialise(self, obj: Objective):
+                    parent_population = []
+                    for i in range(self.Lambda):
+                        params = list(obj.random_params())
+                        genome = Genome(params)
+                        parent_population.append(genome)
+                    self.parent_population = parent_population
+
+                def Mutate(self, objective: Objective):
+                    for i in range(len(self.offspring_population)):
+                        individual = self.mutate(self.offspring_population[i], objective)
+                        self.offspring_population[i] = individual
+
+                def mutate(self, individual: Genome, objective: Objective) -> Genome:
+                    params = self.update_params(individual.params, objective)
+                    return_individual = Genome(params)
+                    return return_individual
+
+                def Crossover(self):
+                    offspring_population = []
+                    population = self.parent_population[:]
+                    random.shuffle(population)
+                    for i in range(len(population)):
+                        individual = self.crossover(population[i], population[(i + 1) % len(population)])
+                        offspring_population.append(individual)
+                    self.offspring_population = offspring_population
 
 
-                    def update_params(self, params) -> tuple[list[float], list[float]]:
-                        params = deepcopy(params)
-                        if isinstance(params, Array):
-                            params = params.to_list()
-                        for i in range(len(params)):
-                            if random.random() < self.mutation_probability:
-                                lo, hi = self.bounds[i]
-                                new_param = lo + (random.random() * (hi - lo))
-                                params[i] = new_param
+                def crossover(self, parent1: Genome, parent2: Genome):
+                    crossover_point = random.randint(1, len(parent1.params) // 2)
+                    params = parent1.params[:crossover_point] + parent2.params[crossover_point:]
+                    return Genome(params)
 
-                        return params
+                def mutationrate(self, x):
+                    return 2 * self.mutation_probability * (1 / (1 + np.e ** ((x)/(self.mutation_probability))))
 
-                    def Selection(self):
-                        solutions = self.parent_population + self.offspring_population
-                        solutions = sorted(solutions, key= lambda x: x.fitness)
-                        self.parent_population = solutions[:self.Lambda]
-                        self.offspring_population = []
+                def update_params(self, params, obj: Objective) -> list[float]:
+                    params = deepcopy(params)
+                    if isinstance(params, Array):
+                        params = params.to_list()
+                    std = np.std(list(map(lambda x: x.fitness, self.parent_population)))
+                    mu = np.mean(list(map(lambda x: x.fitness, self.parent_population)))
+                    mutation_probability = self.mutationrate(std/mu)
+                    for i in range(len(params)):
+                        if random.random() < mutation_probability:
+                            lo, hi = self.bounds[i]
+                            params[i] = random.triangular(lo, hi, params[i])
+                    return params
 
-                    def Evaluation(self, population: list[Genome], obj):
-                        for genome in population:
-                            x = jnp.asarray(genome.params)
-                            loss = float(obj.value(x))
-                            print(f'\t\tLoss: {loss}.')
-                            genome.fitness = loss
+                def Selection(self):
+                    solutions = self.parent_population + self.offspring_population
+                    solutions = sorted(solutions, key= lambda x: x.fitness)
+                    self.parent_population = solutions[:self.Lambda]
+                    self.offspring_population = []
+
+                def Evaluation(self, population: list[Genome], obj):
+                    for genome in population:
+                        x = jnp.asarray(genome.params)
+                        loss = float(obj.value(x))
+                        genome.fitness = loss
 
 
-                    def optimize(
-                        self,
-                        objective: Objective,
-                        init_params: Float[Array, "..."] | None = None,
-                        random_seed: int | None = None,
-                        patience: int | None = None,
-                        mutation_probability: float = 0.5,
-                        std: float = 2
-                    ) -> None:
-                        obj = objective
-                        self.mutation_probability = mutation_probability
-                        self.std = std
+                def optimize(
+                    self,
+                    objective: Objective,
+                    init_params: Float[Array, "..."] | None = None,
+                    random_seed: int | None = None,
+                    patience: int | None = None,
+                    mutation_probability: float = 0.5,
+                ) -> None:
+                    obj = objective
+                    self.mutation_probability = mutation_probability
 
-                        self.prepare(obj, unbounded=False, random_seed=random_seed)
+                    self.prepare(obj, unbounded=False, random_seed=random_seed)
 
-                        obj.warmup_value()
-                        obj.start_logging()
+                    obj.warmup_value()
+                    obj.start_logging()
 
-                        # Repeatedly draw random params from the active (bounded) space and
-                        # evaluate them until the budget is exhausted. Each `obj.value()` call
-                        # is automatically logged.
-                        self.bounds = list(zip(obj.bounds[0], obj.bounds[1]))
+                    # Repeatedly draw random params from the active (bounded) space and
+                    # evaluate them until the budget is exhausted. Each `obj.value()` call
+                    # is automatically logged.
+                    self.bounds = list(zip(obj.bounds[0], obj.bounds[1]))
 
-                        generation = 1
-                        self.Initialise(obj)
-                        self.Evaluation(self.parent_population, obj)
-                        while not obj.budget_exceeded:
-                            print(f"Generation {generation}....")
-                            self.Crossover()
-                            self.Mutate()
-                            self.Evaluation(self.offspring_population, obj)
-                            self.Selection()
-                            generation += 1
-                        return obj.best_params_bounded, obj.best_loss
+                    generation = 1
+                    self.Initialise(obj)
+                    self.Evaluation(self.parent_population, obj)
+                    while not obj.budget_exceeded:
+                        print(f"Generation {generation}....")
+                        self.Crossover()
+                        self.Mutate(obj)
+                        self.Evaluation(self.offspring_population, obj)
+                        self.Selection()
+                        generation += 1
+                    return obj.best_loss
             ```
         """)
 
-        self.format_prompt = textwrap.dedent('''
+        self.format_prompt = textwrap.dedent("""
+            Do not import `OptimizationAlgorithm`, it will be provided in the solution harness.
             Always respond in the following format:
 
             # Description:
@@ -169,9 +172,4 @@ class GravitationalWaveBase(Problem):
             ```python
                 <code>
             ```
-            ''')
-
-
-if __name__ == '__main__':
-    o = GravitationalWaveBase()
-    print(o.get_prompt())
+            """)
---
+++ iohblade/benchmarks/gravitational_waves/uifo_problem.py
@@ -0,0 +1,289 @@
+import textwrap
+
+from typing import Optional
+from dfbench.problems import UIFOProblem
+from dfbench import Objective
+from llamea.feature_guidance import Solution
+from iohblade.misc.prepare_namespace import prepare_namespace
+from iohblade.benchmarks.gravitational_waves.base import GravitationalWaveBase
+
+
+class UIFOProblemSolver(GravitationalWaveBase):
+    """
+    A gravitational wave detection problem based on Quasi-Universal Interferometer (UIFO).
+
+    ## Params
+     * `size: int(3)`: Grid dimensions (3 = 3×3). Larger grids have more components and parameters.
+     * `n_frequencies: int(50)`: Frequency points for sensitivity calculation.
+     * `topology_seed: int(42)`:	Seed for random topology generation. Set to None (with no other topology args) to generate a truly random topology. The seed is always printed to the console. Mutually exclusive with topology and centers/boundaries.
+     * `topology: str? (None)`:	Compact topology string (see below). Mutually exclusive with topology_seed.
+     * `centers: dict? (None)`:	Interior cell dict. Must be paired with boundaries. Mutually exclusive with topology_seed and topology.
+     * `boundaries: dict? (None)`: Boundary cell dict. Must be paired with centers. Mutually exclusive with topology_seed and topology.
+     * `power_penalty_fn: Callable (squashed_relu_penalty)`: Per-element penalty function fn(value, threshold).
+     * `signal_floor: float(1e-20)`: Lower floor for detector signal magnitudes before sensitivity normalization.
+     * `duration: int(15 * 60)`: Budget for the evaluation of the function; officially it is set to 4 hours, set 15 mins for testing.
+    """
+
+    def __init__(
+        self,
+        size=3,
+        n_frequencies=50,
+        topology_seed=42,
+        topology: Optional[str] = None,
+        centers: Optional[dict] = None,
+        boundaries: Optional[dict] = None,
+        power_penalty_fn=None,
+        signal_floor=1e-20,
+        duration: int = 15 * 60,
+    ):
+        super().__init__()
+        self.name += "_UIFOProblem"
+        self.imports = textwrap.dedent("""
+            from dfbench import Objective, OptimizationAlgorithm
+
+            """)
+        self.size = size
+        self.n_frequencies = n_frequencies
+        self.topology_seed = topology_seed
+        self.topology = topology
+        self.centers = centers
+        self.boundaries = boundaries
+        self.power_penalty_fn = power_penalty_fn
+        self.signal_floor = signal_floor
+        self.duration = duration
+        self.minimisation = True
+
+    def evaluate(self, solution: Solution):
+        code = solution.code
+        name = solution.name
+        ns = {}
+        try:
+            ns = prepare_namespace(self.imports + code, self.dependencies)
+            compiled_code = compile(
+                self.imports + code, filename="LLM_Code_" + name, mode="exec"
+            )
+            exec(compiled_code, ns, ns)
+
+            solver = ns[name]
+            if self.power_penalty_fn is not None:
+                uifo_problem = UIFOProblem(
+                    size=self.size,
+                    n_frequencies=self.n_frequencies,
+                    topology_seed=self.topology_seed,
+                    topology=self.topology,
+                    centers=self.centers,
+                    boundaries=self.boundaries,
+                    power_penalty_fn=self.power_penalty_fn,
+                    signal_floor=self.signal_floor,
+                )
+            else:
+                uifo_problem = UIFOProblem(
+                    size=self.size,
+                    n_frequencies=self.n_frequencies,
+                    topology_seed=self.topology_seed,
+                    topology=self.topology,
+                    centers=self.centers,
+                    boundaries=self.boundaries,
+                    signal_floor=self.signal_floor,
+                )
+
+            obj = Objective(
+                uifo_problem,
+                unbounded=True,
+                verbose=1,
+                max_time=self.duration,
+                print_every=10,  # Adapt this to your needs (per n evaluations)
+                save_params_history=True,
+                save_to_file_every=100,
+                display_mode="log",  # Use "live" for a live display on an interactive terminal
+            )
+
+            solver_object = solver()
+
+            best_loss = solver_object.optimize(obj)
+            solution.set_scores(
+                best_loss, f"Got best loss of {best_loss} under {self.duration} s."
+            )
+            # Output run history to ~/data/*
+            obj.save_run_data(hyper_param_str=solution.id)
+            obj.output_to_files(hyper_param_str=solution.id)
+        except Exception as e:
+            solution.set_scores(float("inf"), f"Got error {e}.", e)
+        return solution
+
+    def get_config(self):
+        import inspect
+
+        evaluator = inspect.getsource(self.evaluate)
+        extra_config = {
+            "size": self.size,
+            "n_frequencies": self.n_frequencies,
+            "topology_seed": self.topology_seed,
+            "topology": self.topology,
+            "centers": self.centers,
+            "boundaries": self.boundaries,
+            "power_penalty_fn": (
+                self.power_penalty_fn.__name__
+                if self.power_penalty_fn is not None
+                else "squashed_relu_penalty"
+            ),
+            "signal_floor": self.signal_floor,
+            "duration": self.duration,
+        }
+        config = {
+            "tags": [
+                "physics",
+                "gravitational waves",
+                "frontier physics",
+                "many dimension optimisation",
+            ],
+            "name": "Gravitational Wave Detector Optimisation: Quasi-Universal Interferometer (UIFO)",
+            "prompt": self.get_prompt(),
+            "minimisation": self.minimisation,
+            "evaluator": evaluator,
+            "config": extra_config,
+        }
+        return config
+
+    def test(self, solution):
+        return self.evaluate(solution)
+
+    def to_dict(self):
+        return self.__dict__.copy()
+
+
+if __name__ == "__main__":
+    uifo_ad = UIFOProblemSolver()
+    solution = Solution(textwrap.dedent("""
+        from dataclasses import dataclass
+        import random
+        from copy import deepcopy
+        import numpy as np
+        import jax.numpy as jnp
+        from jaxtyping import Array, Float
+        @dataclass
+        class Genome:
+            params : list[float]
+            speed : list[float]
+            fitness: float = float('nan')
+
+            def reset_fitness(self):
+                self.fitness = float('nan')
+
+
+        class GeneticAlgorithm(OptimizationAlgorithm):
+
+            algorithm_str = 'GeneticAlgorithm'
+
+            def __init__(self, Lambda=10, Mu=10) -> None:
+                super()
+                self.parent_population: list[Genome] = []
+                self.offspring_population: list[Genome] = []
+                self.Lambda = Lambda
+                self.Mu = Mu
+
+            def Initialise(self, obj: Objective):
+                parent_population = []
+                for i in range(self.Lambda):
+                    params = list(obj.random_params())
+                    genome = Genome(params, [0] * len(params))
+                    parent_population.append(genome)
+                self.parent_population = parent_population
+
+            def Mutate(self, objective: Objective):
+                for i in range(len(self.offspring_population)):
+                    individual = self.mutate(self.offspring_population[i], objective)
+                    self.offspring_population[i] = individual
+
+            def mutate(self, individual: Genome, objective: Objective) -> Genome:
+                params, speed = self.update_params(individual.params, individual.speed, objective)
+                return_individual = Genome(params, speed)
+                return return_individual
+
+            def Crossover(self):
+                offspring_population = []
+                population = self.parent_population[:]
+                random.shuffle(population)
+                for i in range(len(population)):
+                    individual = self.crossover(population[i], population[(i + 1) % len(population)])
+                    offspring_population.append(individual)
+
+                self.offspring_population = offspring_population
+
+
+            def crossover(self, parent1: Genome, parent2: Genome):
+                crossover_point = random.randint(1, len(parent1.params) // 2)
+                params = parent1.params[:crossover_point] + parent2.params[crossover_point:]
+                speed = parent1.speed[:crossover_point] + parent2.speed[crossover_point:]
+                return Genome(params, speed)
+
+            def mutationrate(self, x):
+                return 2 * self.mutation_probability * (1 / (1 + np.e ** ((x)/(self.mutation_probability))))
+
+            def update_params(self, params, speed, obj: Objective) -> tuple[list[float],list[float]]:
+                params = deepcopy(params)
+                speed = deepcopy(speed)
+                if isinstance(params, Array):
+                    params = params.to_list()
+                std = np.std(list(map(lambda x: x.fitness, self.parent_population)))
+                mu = np.mean(list(map(lambda x: x.fitness, self.parent_population)))
+                mutation_probability = self.mutationrate(std/mu)
+                print(f'\t Mutation probability: {mutation_probability}, parent population standard deviation {std}.')
+                for i in range(len(params)):
+                    if random.random() < mutation_probability:
+                        speed[i] = random.uniform(speed[i], self.std)
+                        params[i] *= (np.e ** speed[i])
+                return params, speed
+
+            def Selection(self):
+                solutions = self.parent_population + self.offspring_population
+                solutions = sorted(solutions, key= lambda x: x.fitness)
+                self.parent_population = solutions[:self.Lambda]
+                self.offspring_population = []
+
+            def Evaluation(self, population: list[Genome], obj):
+                for genome in population:
+                    x = jnp.asarray(genome.params)
+                    loss = float(obj.value(x))
+                    print(f'\t\tLoss: {loss}{"*" if loss < max(list(map(lambda x: x.fitness, self.parent_population))) else ""}')
+                    genome.fitness = loss
+
+
+            def optimize(
+                self,
+                objective: Objective,
+                init_params: Float[Array, "..."] | None = None,
+                random_seed: int | None = None,
+                patience: int | None = None,
+                mutation_probability: float = 0.5,
+                std: float = 2
+            ) -> None:
+                obj = objective
+                self.mutation_probability = mutation_probability
+                self.std = std
+                self.prepare(obj, unbounded=False, random_seed=random_seed)
+
+                obj.warmup_value()
+                obj.start_logging()
+
+                # Repeatedly draw random params from the active (bounded) space and
+                # evaluate them until the budget is exhausted. Each `obj.value()` call
+                # is automatically logged.
+                self.bounds = list(zip(obj.bounds[0], obj.bounds[1]))
+
+                generation = 1
+                self.Initialise(obj)
+                self.Evaluation(self.parent_population, obj)
+                while not obj.budget_exceeded:
+                    print(f"Generation {generation}....")
+                    self.Crossover()
+                    self.Mutate(obj)
+                    self.Evaluation(self.offspring_population, obj)
+                    self.Selection()
+                    generation += 1
+                return obj.best_loss
+        """))
+    solution.name = "GeneticAlgorithm"
+    solution = uifo_ad.evaluate(solution)
+    print(uifo_ad.imports + solution.code)
+    print(solution.fitness, solution.feedback, solution.error)
